4554a959e89f005a904933c9bfe8ba92f0e42dae
augusalterats <augusalterats@gmail.com>
2024-12-04 18:04:27 -0300

    commit 14

--- script/funciones.js
+++ script/funciones.js
@@ -6,13 +6,27 @@ const text2 = 'La Liga vence a Abu Castor por 63 a 55 en la primera fecha del To
 const arrayFixture = [
   ['j1p1', 'Viernes, 29/11 - 21:00', '2024-11-29T21:00', 'KIOSCO J.', 70, '04 Guillermo Godoy (25)', '4M DIST.', 54, '08 José Martín Borda (11)'],
   ['j1p2', 'Viernes, 29/11 - 22:30', '2024-11-29T22:30', 'LA LIGA', 63, '01 Mariano Y. Romero (17)', 'ABU CASTOR', 55, '06 Néstor Altamirano (15)'],
+
   ['j2p1', 'Miércoles, 04/12 - 21:00', '2024-12-04T21:00', 'KIOSCO J.', '--', '', 'LA LIGA', '--', ''],
   ['j2p2', 'Miércoles, 04/12 - 22:30', '2024-12-04T22:30', 'EL BUNKER', '--', '', 'GOYIN', '--', ''],
+
   ['j3p1', 'Viernes, 06/12 - 21:00', '2024-12-06T21:00', 'ABU CASTOR', '--', '', 'GOYIN', '--', ''],
   ['j3p2', 'Viernes, 06/12 - 22:30', '2024-12-06T22:30', '4M DIST.', '--', '', 'EL BUNKER', '--', ''],
-  ['j4p1', 'Domingo, 08/12 - 20:00', '2024-12-08T20:00', 'KIOSCO J.', '--', '', 'ABU CASTOR', '--', ''],
-  ['j4p2', 'Domingo, 08/12 - 21:30', '2024-12-08T21:30', '4M DIST.', '--', '', 'LA LIGA', '--', '']
 
+  ['j4p1', 'Miércoles, 11/12 - 21:00', '2024-12-11T21:00', 'KIOSCO J.', '--', '', 'ABU CASTOR', '--', ''],
+  ['j4p2', 'Miércoles, 11/12 - 22:30', '2024-12-11T22:30', '4M DIST.', '--', '', 'LA LIGA', '--', ''],
+
+  ['j5p1', 'Viernes, 13/12 - 21:00', '2024-12-13T21:00', 'LA LIGA.', '--', '', 'EL BUNKER', '--', ''],
+  ['j5p2', 'Viernes, 13/12 - 22:30', '2024-12-13T22:30', 'KIOSCO J.', '--', '', 'GOYIN', '--', ''],
+
+  ['j6p1', 'Domingo, 15/12 - 20:00', '2024-12-15T20:00', 'ABU CASTOR', '--', '', 'EL BUNKER', '--', ''],
+  ['j6p2', 'Domingo, 15/12 - 21:30', '2024-12-15T21:30', '4M DIST.', '--', '', 'GOYIN', '--', ''],
+
+  ['j7p1', 'Miércoles, 18/12 - 21:00', '2024-12-18T21:00', 'LA LIGA', '--', '', 'GOYIN', '--', ''],
+  ['j7p2', 'Miércoles, 18/12 - 22:30', '2024-12-18T22:30', 'KIOSCO J.', '--', '', 'EL BUNKER', '--', ''],
+
+  ['j8p1', 'Miércoles, 18/12 - 21:00', '2024-12-18T21:00', '4M DIST.', '--', '', 'ABU CASTOR', '--', ''],
+  // ['j8p2', 'Miércoles, 18/12 - 22:30', '2024-12-18T22:30', '4M DIST.', '--', '', 'LA LIGA', '--', '']
 ];
 
 
